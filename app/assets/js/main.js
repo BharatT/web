4206fb6be563e6e1f1d80a7f1e030500a541a669
Bharat <btiwari033@gmail.com>
2019-01-07 23:53:32 +0530

    Function implementation on jquery

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,12 +1,42 @@
 $(document).ready(function(){
-    $('.example-photo').css("opacity",0)
-    
-    $(window).scroll(function(){
-        var windowHeight= $(window).height();
+
+       var windowHeight= $(window).height();
        var windowScrollPosTop = $(window).scrollTop();
        var windowScrollPosBottom = windowHeight + windowScrollPosTop
-       
 
+$.fn.revealOnScroll=function(){
+    return (this).each(() => {
+       //$(this).hide();
+if(!$(this).hasClass('hidden')){
+       $(this).css("opacity",0).addClass('hidden');
+    }
+       var objectOffset = $(this).offset();
+       var objectOffsetTop = objectOffset.top;
+
+if(!$(this).hasClass('animation-complete')){
+    if(windowScrollPosBottom > objectOffsetTop){
+        $(this).animate({"opacity":1}, 3000).addClass("animation-complete");
+   }
+}
+        
+    });
+
+}//end our function here....
+
+
+$("#features").revealOnScroll();
+
+$('.example-photo').revealOnScroll();
+
+
+    // $('.example-photo').css("opacity",0)
+    
+    $(window).scroll(function(){
+        windowHeight= $(window).height();
+        windowScrollPosTop = $(window).scrollTop();
+        windowScrollPosBottom = windowHeight + windowScrollPosTop
+       
+/* 
        var objectOffset = $('.example-photo').offset();
        var objectOffsetTop = objectOffset.top;
 
@@ -14,9 +44,9 @@ if(!$('.example-photo').hasClass('animation-complete')){
     if(windowScrollPosBottom > objectOffsetTop){
         $('.example-photo').animate({"opacity":1}, 3000).addClass("animation-complete");
    }
-}
+} */
       
-       console.log(objectOffsetTop);
+       //console.log(objectOffsetTop);
 
     })
 });
